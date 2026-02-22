cfa7732602098f4ade8140356c4f0363ae4d359e
BrianPech99 <BrianJosepPech@gmail.com>
2026-02-21 20:42:50 -0500

    Editado mi codigo

--- Prueba_de_Concepto.ino
+++ Prueba_de_Concepto.ino
@@ -1,35 +1,39 @@
 #include <DigiKeyboard.h>
 
 void setup() {
-  DigiKeyboard.delay(3000);
-
-  // Cambiar a teclado EN (NO SE QUITA)
-  DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_GUI_LEFT);
-  DigiKeyboard.delay(1500);
+  DigiKeyboard.delay(4000);
 
   // Win + R
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(800);
+
+  // CMD
+  DigiKeyboard.print("cmd");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2500);
+
+  // Ir al escritorio
+  DigiKeyboard.print("cd %USERPROFILE%\\Desktop");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(1000);
 
-  // Abrir CMD
-  DigiKeyboard.print("cmd");
+  // Descargar imagen
+  DigiKeyboard.print("curl https://i.pinimg.com/236x/b0/f0/52/b0f0524e80ed52cf5a38d0fa93035a95.jpg -o g.jpg");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(3000);
 
-  // Ir al Escritorio
-  DigiKeyboard.print("cd %USERPROFILE%\\Desktop");
+  // SONIDO CONTINUO EN SEGUNDO PLANO (~10s)
+  DigiKeyboard.print("start /b powershell -c \"for($i=0;$i -lt 20;$i++){(New-Object Media.SoundPlayer 'C:\\Windows\\Media\\Windows Hardware Remove.wav').PlaySync()}\"");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(2000);
+  DigiKeyboard.delay(200); // no espera
 
-  // Descargar imagen
-  DigiKeyboard.print(
-    "curl https://i.pinimg.com/236x/b0/f0/52/b0f0524e80ed52cf5a38d0fa93035a95.jpg -o gato.jpg"
-  );
+  // Abrir imagen 20 veces (mientras suena)
+  DigiKeyboard.print("for /L %i in (1,1,20) do start g.jpg");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(5000);
+  DigiKeyboard.delay(1000);
 
-  // Abrir la imagen 3 veces
-  DigiKeyboard.print("for /L %i in (1,1,3) do start \"\" gato.jpg");
+  // Cerrar CMD
+  DigiKeyboard.print("exit");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
 }
 
